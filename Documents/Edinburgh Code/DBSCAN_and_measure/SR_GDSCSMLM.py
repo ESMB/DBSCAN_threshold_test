89af508379977ea289207d18443dc401c084cf6b
Mathew <Mathew@c066030.wlan.net.ed.ac.uk>
2022-03-21 12:56:45 +0000

    First commit.

--- Documents/Edinburgh Code/DBSCAN_and_measure/SR_GDSCSMLM.py
+++ Documents/Edinburgh Code/DBSCAN_and_measure/SR_GDSCSMLM.py
@@ -12,42 +12,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from sklearn.cluster import DBSCAN
+from skimage import filters,measure
 
+# Camera settings
+Pixel_size=103.0
+camera_gain=250
+camera_sensitivity=11.5
+camera_offset=500
 
-     
+# Options (set to 1 to perform)
+fit=0
+link=0
+torender=1
+to_cluster=1       
 
 # Settings
-image_width=512
-image_height=512
-Pixel_size=103
+image_width=684
+image_height=428
+# image_width=512
+# image_height=512
+gradient=7500
+drift_correction=0
 scale=8
-
-# Thresholds to try
-# This is the distance threshold (pixels)
-eps=[]          
-
-
-eps.append(1)
-eps.append(2)
-
-
-# This is the minimum number of locs per cluster
-
-minimum_locs=[]
-
-minimum_locs.append(50)
-
-
+precision_threshold=250
+eps_threshold=0.5
+minimum_locs_threshold=50
+prec_thresh=40
 
 filename_contains="FitResults"
 
-
-
-# Files to analyse
-
+# Folders to analyse:
+root_path=r"/Users/Mathew/Documents/Current analysis/Craig files/"
 pathList=[]
 
-pathList.append(r"/Users/Mathew/Documents/Current analysis/Actin LIVE-PAINT/")
+
+
+
+
+pathList.append(r"/Users/Mathew/Documents/Current analysis/Craig files/")
         
 # Various functions
 
@@ -67,17 +69,38 @@ def generate_SR(coords):
         j+=1
     return SR_plot_def
 
-# This is to generate a gaussian.
-def gkern(l,sigx,sigy):
+def SRGaussian(size, fwhm, center):
 
+    sizex=size[0]
+    sizey=size[1]
+    x = np.arange(0, sizex, 1, float)
+    y = x[0:sizey,np.newaxis]
+    # y = x[:,np.newaxis]
+
+
+    x0 = center[0]
+    y0 = center[1]
+    
+    wx=fwhm[0]
+    wy=fwhm[1]
+    
+    return np.exp(-0.5 * (np.square(x-x0)/np.square(wx) + np.square(y-y0)/np.square(wy)) )
+
+def gkern(l,sigx,sigy):
+    """\
+    creates gaussian kernel with side length l and a sigma of sig
+    """
+
+    # ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
     ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
     xx, yy = np.meshgrid(ax, ax)
 
     kernel = np.exp(-0.5 * (np.square(xx)/np.square(sigx) + np.square(yy)/np.square(sigy)) )
-
+    # print(np.sum(kernel))
+    # test=kernel/np.max(kernel)
+    # print(test.max())
     return kernel/np.sum(kernel)
 
-# Generate image with width = precision
 def generate_SR_prec(coords,precsx,precsy):
     box_size=20
     SR_prec_plot_def=np.zeros((image_width*scale,image_height*scale),dtype=float)
@@ -125,7 +148,7 @@ def generate_SR_prec(coords,precsx,precsy):
 
 # Perform DBSCAN on the coordinates. 
 
-def cluster(coords,eps_threshold,minimum_locs_threshold):
+def cluster(coords):
      db = DBSCAN(eps=eps_threshold, min_samples=minimum_locs_threshold).fit(coords)
      labels = db.labels_
      n_clusters_ = len(set(labels)) - (1 if-1 in labels else 0)  # This is to calculate the number of clusters.
@@ -211,10 +234,23 @@ def generate_SR_prec_cluster(coords,precsx,precsy,clusters):
     
     return SR_prec_plot,labelled,SR_fwhm_plot_def
    
+def analyse_labelled_image(labelled_image):
+    
+    measure_image=measure.regionprops_table(labelled_image,intensity_image=labelled_image,properties=('area','perimeter','centroid','orientation','major_axis_length','minor_axis_length','mean_intensity','max_intensity'))
+    measure_dataframe=pd.DataFrame.from_dict(measure_image)
+    return measure_dataframe
+
+if to_cluster==1:
+    # To store overall medians/means etc. 
+    Output_all_cases = pd.DataFrame(columns=['Path','Number_of_clusters','Points_per_cluster_mean','Points_per_cluster_SD','Points_per_cluster_med',
+                                       'Area_mean','Area_sd','Area_med','Length_mean','Length_sd','Length_med','Ratio_mean','Ratio_sd','Ratio_med'])
 
 
 for path in pathList:
     print(path)
+    path=path+"/"
+
+    # Perform the fitting
 
     # Load the fits:
     for root, dirs, files in os.walk(path):
@@ -227,8 +263,12 @@ for path in pathList:
     
     fits_path=path+resultsname
     
-    loc_data = pd.read_table(fits_path,sep=",")
+    loc_data = pd.read_table(fits_path)
     
+    index_names = loc_data[loc_data['Precision (nm)']>prec_thresh].index
+    loc_data.drop(index_names, inplace = True)
+   
+    path=path+"GDSCSMLM_"
 
     # Extract useful data:
     coords = np.array(list(zip(loc_data['X'],loc_data['Y'])))
@@ -258,65 +298,132 @@ for path in pathList:
     imsr = Image.fromarray(SR_prec)
     imsr.save(path+'SR_width_python.tif')
     
-
-# Cluster analysis:
+    # Cluster analysis
+    if to_cluster==1:
+        clusters=cluster(coords)
     
-    for e in eps:
-        for locs in minimum_locs:
-            clusters=cluster(coords,e,locs)
+        # Check how many localisations per cluster
+     
+        cluster_list=clusters.tolist()    # Need to convert the dataframe into a list- so that we can use the count() function. 
+        maximum=max(cluster_list)+1  
+        
+        
+        cluster_contents=[]         # Make a list to store the number of clusters in
+        
+        for i in range(0,maximum):
+            n=cluster_list.count(i)     # Count the number of times that the cluster number i is observed
+           
+            cluster_contents.append(n)  # Add to the list. 
+        
+        if len(cluster_contents)>0:
+            average_locs=sum(cluster_contents)/len(cluster_contents)
+     
+            plt.hist(cluster_contents, bins = 20,range=[0,200], rwidth=0.9,color='#607c8e') # Plot a histogram. 
+            plt.xlabel('Localisations per cluster')
+            plt.ylabel('Number of clusters')
+            plt.savefig('path+Localisations.pdf')
+            plt.show()
             
-            path2save=path+"e_"+str(e)+"_locs_"+str(locs)+""
-            # Check how many localisations per cluster
-         
-            cluster_list=clusters.tolist()    # Need to convert the dataframe into a list- so that we can use the count() function. 
-            maximum=max(cluster_list)+1  
+            cluster_arr=np.array(cluster_contents)
+        
+            median_locs=np.median(cluster_arr)
+            mean_locs=cluster_arr.mean()
+            std_locs=cluster_arr.std()
             
+        
+            # Generate the SR image.
+            SR_Clu=generate_SR_cluster(coords,clusters)
             
-            cluster_contents=[]         # Make a list to store the number of clusters in
+            imsr = Image.fromarray(SR_Clu)
+            imsr.save(path+'SR_points_python_clustered.tif')
             
-            for i in range(0,maximum):
-                n=cluster_list.count(i)     # Count the number of times that the cluster number i is observed
-               
-                cluster_contents.append(n)  # Add to the list. 
-            
-            if len(cluster_contents)>0:
-                average_locs=sum(cluster_contents)/len(cluster_contents)
-         
-                plt.hist(cluster_contents, bins = 20,range=[0,200], rwidth=0.9,color='#607c8e') # Plot a histogram. 
-                plt.xlabel('Localisations per cluster')
-                plt.ylabel('Number of clusters')
-                plt.savefig(path2save+'Localisations.pdf')
-                plt.show()
-                
-                cluster_arr=np.array(cluster_contents)
-            
-                median_locs=np.median(cluster_arr)
-                mean_locs=cluster_arr.mean()
-                std_locs=cluster_arr.std()
-                
-            
-                # Generate the SR image.
-                SR_Clu=generate_SR_cluster(coords,clusters)
-                
-                imsr = Image.fromarray(SR_Clu)
-                imsr.save(path2save+'SR_points_python_clustered.tif')
-                
-                SR_clu_prec,labelled,SR_prec_plot=generate_SR_prec_cluster(coords,precsx,precsy,clusters)
-                
-                
-
-            
-                imsr = Image.fromarray(SR_clu_prec)
-                imsr.save(path2save+'SR_width_python_clustered.tif')
-                
-                imsr = Image.fromarray(labelled)
-                imsr.save(path2save+'SR_fwhm_python_clustered.tif')
-                
-                labeltot=labelled.max()+1
-            
-                print('Total number of clusters in labelled image: %d'%labeltot)
-                
-                
-                
+            SR_clu_prec,labelled,SR_prec_plot=generate_SR_prec_cluster(coords,precsx,precsy,clusters)
             
             
+            
+            imsr = Image.fromarray(SR_prec_plot)
+            imsr.save(path+'Test_prec.tif')
+            
+        
+     
+        
+            
+            imsr = Image.fromarray(SR_clu_prec)
+            imsr.save(path+'SR_width_python_clustered.tif')
+            
+            imsr = Image.fromarray(labelled)
+            imsr.save(path+'SR_fwhm_python_clustered.tif')
+            
+            labeltot=labelled.max()+1
+        
+            print('Total number of clusters in labelled image: %d'%labeltot)
+            
+            
+            labelled_to_analyse=labelled.astype('int')
+            measurements=analyse_labelled_image(labelled_to_analyse)
+        
+        
+            # Make and save histograms
+            
+            areas=measurements['area']*((Pixel_size/(scale*1000))**2)
+            plt.hist(areas, bins = 20,range=[0,0.1], rwidth=0.9,color='#ff0000')
+            plt.xlabel('Area (\u03bcm$^2$)',size=20)
+            plt.ylabel('Number of Features',size=20)
+            plt.title('Cluster area',size=20)
+            plt.savefig(path+"Area.pdf")
+            plt.show()
+            
+            median_area=areas.median()
+            mean_area=areas.mean()
+            std_area=areas.std()
+            
+            
+            length=measurements['major_axis_length']*((Pixel_size/8))
+            plt.hist(length, bins = 20,range=[0,1000], rwidth=0.9,color='#ff0000')
+            plt.xlabel('Length (nm)',size=20)
+            plt.ylabel('Number of Features',size=20)
+            plt.title('Cluster lengths',size=20)
+            plt.savefig(path+"Lengths.pdf")
+            plt.show()
+        
+            median_length=length.median()
+            mean_length=length.mean()
+            std_length=length.std()
+            
+            ratio=measurements['minor_axis_length']/measurements['major_axis_length']
+            plt.hist(ratio, bins = 50,range=[0,1], rwidth=0.9,color='#ff0000')
+            plt.xlabel('Eccentricity',size=20)
+            plt.ylabel('Number of Features',size=20)
+            plt.title('Cluster Eccentricity',size=20)
+            plt.savefig(path+"Ecc.pdf")
+            plt.show()
+            
+            median_ratio=ratio.median()
+            mean_ratio=ratio.mean()
+            std_ratio=ratio.std()
+            
+            measurements['Eccentricity']=ratio
+            # if len(measurements)==len(cluster_contents):
+            measurements['Number_of_locs']=cluster_contents
+            measurements.to_csv(path + 'Metrics_40.csv', sep = '\t')
+            
+            Output_overall = pd.DataFrame(columns=['xw','yw','cluster'])
+            
+            Output_overall['xw']=xcoords
+            
+            Output_overall['yw']=ycoords
+            
+            Output_overall['cluster']=clusters
+            
+            Output_overall.to_csv(path + 'all.csv', sep = '\t')    
+        
+            Output_all_cases = Output_all_cases.append({'Path':path,'Number_of_clusters':maximum,'Points_per_cluster_mean':mean_locs,'Points_per_cluster_SD':std_locs,'Points_per_cluster_med':median_locs,
+                                                'Area_mean':mean_area,'Area_sd':std_area,'Area_med':median_area,'Length_mean':mean_length,'Length_sd':std_length,'Length_med':median_length,
+                                                'Ratio_mean':mean_ratio,'Ratio_sd':std_ratio,'Ratio_med':median_ratio},ignore_index=True)
+        else:
+            Output_all_cases = Output_all_cases.append({'Path':path,'Number_of_clusters':'0','Points_per_cluster_mean':'0','Points_per_cluster_SD':'0','Points_per_cluster_med':'0',
+                                                'Area_mean':'0','Area_sd':'0','Area_med':'0','Length_mean':'0','Length_sd':'0','Length_med':'0',
+                                                'Ratio_mean':'0','Ratio_sd':'0','Ratio_med':'0'},ignore_index=True)
+       
+        if to_cluster==1:
+            Output_all_cases.to_csv(root_path + 'GDSC_all_metrics_60.csv', sep = '\t')
